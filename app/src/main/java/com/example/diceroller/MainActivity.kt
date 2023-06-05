d5243858eee5959cc4cf7dbf3b6d7b411485be9c
Becca B <cerulean.rebecca20@hotmail.com>
2023-06-05 20:35:35 +0100

    added dice class

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -25,3 +25,10 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
