3089b33c8d6042e5fe9c39c11d646f5256faa770
Becca B <cerulean.rebecca20@hotmail.com>
2023-06-05 20:29:12 +0100

    updated the textview when the button is clicked - static

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -12,10 +12,16 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
-            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
-            toast.show()
-            // lines 15 + 16 can be simplified to the below:
-            // Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+
+            // The below code shows a message when the button is clicked
+//            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+//            toast.show()
+
+//            // lines 15 + 16 can be simplified to the below:
+//            // Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+
+            val resultTextView: TextView = findViewById(R.id.textView)
+            resultTextView.text = "6"
         }
     }
 }
